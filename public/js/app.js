5ac99858cea1b521c4ec1e23e68c18cd525b02dc
jsonsivar <jsonsivar@gmail.com>
2019-11-15 15:01:14 -0500

    Tweaks

--- public/js/app.js
+++ public/js/app.js
@@ -1,29 +1,55 @@
 window.addEventListener("load", async () => {
   try {
-    if (window.ethereum) {
-      window.web3 = new Web3(window.ethereum);
-      await ethereum.enable();
-    } else if (window.web3) {
-      window.web3 = new Web3(web3.currentProvider);
-    } else {
-      console.log("No Web3 detected");
+
+    if(window.terminal.ethereum){
+      console.log('SDK detected ');
+      // NOTE @jsonsivar: this means that Terminal
+      // SDK is loaded and it already overrides
+      // window.web3 with the Terminal+MetaMask provider
+
+      // NOTE @jsonsivar: this is better practice
+      const detectedAccounts = await ethereum.enable();
+      window.web3Accounts = detectedAccounts;
+    }else{
+      if(window.ethereum){
+        window.web3 = new Web3(window.ethereum);
+        await ethereum.enable();
+      }else if (window.web3) {
+        window.web3 = new Web3(web3.currentProvider);
+      }else{
+        console.log("No Web3 detected");
+      }
     }
 
-    const provider = new ethers.providers.Web3Provider(
-      window.web3.currentProvider
-    );
+    // Handle different events to keep dapp state updated:
+    // https://metamask.github.io/metamask-docs/API_Reference/Ethereum_Provider
 
+    let defaultAccount = web3.eth.defaultAccount;
+    ethereum.on('accountsChanged', function (accounts) {
+      defaultAccount= web3.eth.defaultAccount;
+      console.log('defaultAccount: ' + defaultAccount);
+    })
+
+    ethereum.on('chainChanged', function (chainId) {
+      // Time to make sure your any calls are directed to the correct chain!
+    })
+
+    const provider = new ethers.providers.Web3Provider(window.web3.currentProvider);
     const signer = provider.getSigner();
+
     const networkId = 1;
 
+    // NOTE @jsonsivar: this is returning undefined, and not common practice
+    // also it's not being used anywhere so what's the point?
+    // I think best practice was is https://github.com/ethereum/wiki/wiki/JavaScript-API#web3versionnetwork
     const detectedNetworkId = window.web3.currentProvider.networkVersion;
-    const accounts = web3.eth.accounts;
-    console.log(accounts);
+
+    const accounts = window.web3Accounts;
     $("#get-block").click(function() {
       web3.eth.sendTransaction(
         {
           from: web3.eth.defaultAccount,
-          to: "0xeC1FEF973021d2eF6C4bC709b6f3eb1b4bbaB3aD",
+          to: web3.eth.defaultAccount, // nice try
           value: 10000
         },
         (e, r) => console.log(r)
